79dc97c14930a92c251cb2ccb791aa295006c92e
Frank Weinhold <frank.weinhold@devboost.com>
2021-06-10 13:55:48 +0200

    initial commit

---
+++ src/main/java/com/devboost/openapigeneratorspring_nullablemapbug/OpenApiGeneratorSpringNullableMapBugApplication.java
@@ -0,0 +1,13 @@
+package com.devboost.openapigeneratorspring_nullablemapbug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenApiGeneratorSpringNullableMapBugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenApiGeneratorSpringNullableMapBugApplication.class, args);
+	}
+
+}
